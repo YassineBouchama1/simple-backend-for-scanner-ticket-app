c45756cc4e7108cbb89576af7a36d66619074782
siskoweb <sisko4dev@gmail.com>
2024-03-09 23:43:42 +0100

    lazy

--- controllers/ticket.js
+++ controllers/ticket.js
@@ -6,7 +6,7 @@ exports.createTicket = async function(req, res, next) {
         const ticket = await TicketModel.create(req.body);
         res.status(201).json({ data: ticket, message: 'Ticket created' });
     } catch (error) {
-        next(error);
+         return next(new ApiError("There is error", 505));
     }
 }
 
@@ -15,7 +15,7 @@ exports.getTickets = async function(req, res, next) {
         const tickets = await TicketModel.find();
         res.status(200).json({ data: tickets, message: 'Tickets' });
     } catch (error) {
-        next(error);
+         return next(new ApiError("There is error", 505));
     }
 }
 
@@ -31,7 +31,7 @@ exports.showTicket = async function(req, res, next) {
         }
         res.status(200).json({ data: ticket, message: 'Ticket found' });
     } catch (error) {
-        next(error);
+         return next(new ApiError("There is error", 505));
     }
 }
 
@@ -47,7 +47,7 @@ exports.updateTicket = async function(req, res, next) {
         }
         res.status(200).json({ data: ticket, message: 'Ticket updated' });
     } catch (error) {
-        next(error);
+         return next(new ApiError("There is error", 505));
     }
 }
 
@@ -63,6 +63,6 @@ exports.deleteTicket = async function(req, res, next) {
         }
         res.status(200).json({ data: ticket, message: 'Ticket deleted' });
     } catch (error) {
-        next(error);
+         return next(new ApiError("There is error", 505));
     }
 }
